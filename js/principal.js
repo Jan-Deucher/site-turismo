4c0fadbcc79078bca0da819e4525a2207355c906
Janete Deucher <janetedeucher@gmail.com>
2021-07-14 18:35:31 -0300

    add alterações com docker

--- js/principal.js
+++ js/principal.js
@@ -34,3 +34,35 @@ $('.carrosel').slick({
     
     $('.slider-alfredo-wagner').addClass('hide');
   });
+
+  
+  
+  $.get( "http://localhost:8090/product/list", function( produtos ) {
+    for(id in produtos){
+     
+      console.log(produtos[id])      
+      $clone = $("#copyrow").clone( true )
+      $clone.removeAttr("id")
+      $clone.removeAttr("style")
+      console.log($clone)
+      
+      $clone.find("#img-card").attr('src', produtos[id].image);
+      $clone.find("#nome").attr('src', produtos[id].name);
+      $clone.find("#descricao").attr('src', produtos[id].description);
+      $clone.find("#detalhes").attr('src', produtos[id].id);
+      
+      console.log( produtos[id].id)      
+      
+      $("#cards").prepend($clone);
+      $("#descricao").text(produtos[id].description);
+      $("#nome").text(produtos[id].name);
+      $("#detalhes").val(produtos[id].id);
+
+    }
+  });
+
+$.get("http://localhost:8090/product/"), function(){
+    $("#detalhes").val(produtos[id].id);
+}
+
+
